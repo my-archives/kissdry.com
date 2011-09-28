11dee1a3a618b8e32f7745d9a763365d0179d414
cfddream <cfddream@gmail.com>
2011-09-29 03:25:26 +0800

    updated datetime

--- markdown.js
+++ markdown.js
@@ -1,21 +1,25 @@
 var markdown = require('Markdown'),
+    spawn = require('child_process').spawn,
+    date = spawn('date', ['+%Y-%m-%d %H:%M:%S']),
     fs = require('fs'),
     root = __dirname,
     docs = root + '/docs/',
     home = root + '/!/',
-	assets = root + '/assets/',
+    assets = root + '/assets/',
     files = process.argv.slice(2),
     md_stuffix = '.md',
     html_stuffix = '.html',
-	TC_START = [
-		'head.html',
-		'body_header.html',
-		'body_nav.html',
-		'body_container.html'],
-	TC_END = [
-		'body_aside.html',
-		'body_footer.html',
-		'foot.html'];
+    TC_START = [
+        'head.html',
+        'body_header.html',
+        'body_nav.html',
+        'body_container.html'],
+    TC_END = [
+        'body_aside.html',
+        'body_footer.html',
+        'foot.html'],
+    datetime = '';
+
 
 function main() {
     files.forEach(function (v, i) {
@@ -32,17 +36,24 @@ function convert(file) {
         };
         filename = home + file + html_stuffix;
 
-		TC_START.forEach(function (item) {
-			tc_start += fs.readFileSync(assets + item, 'utf-8');
-		});	
+        TC_START.forEach(function (item) {
+            tc_start += fs.readFileSync(assets + item, 'utf-8');
+        });	
 
-		TC_END.forEach(function (item) {
-			tc_end += fs.readFileSync(assets + item, 'utf-8');
-		});	
+        TC_END.forEach(function (item) {
+            tc_end += fs.readFileSync(assets + item, 'utf-8');
+        });	
 
-        fs.writeFile(filename, tc_start + markdown.parse(data) + tc_end, function (err) {
-            if (err) throw err;
-            console.log(file + '.md to ' + file + '.html', 'saved.');
+        date.stdout.setEncoding('utf8');
+        date.stdout.on('data', function (data) {
+            datetime = data.trim();
+
+            fs.writeFile(filename, tc_start.replace(/@DATE@/, datetime)
+                         + markdown.parse(data)
+                         + tc_end, function (err) {
+                if (err) throw err;
+                console.log(file + '.md to ' + file + '.html', 'saved.');
+            });
         });
     }); 
 }

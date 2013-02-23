2c4e532d6a26d732156f76c9bdd1d74106c80857
cfddream <cfddream@gmail.com>
2013-02-23 16:38:15 +0800

    inited

---
+++ content/hello.md
@@ -0,0 +1,3 @@
+Title: Hello Pelican
+Date: 2013-02-23
+Slug: hello-pelican
---
+++ pelicanconf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'cfd'
+SITENAME = u'!#/bin/cfd'
+SITEURL = 'kissdry.com'
+TIMEZONE = 'Asia/Shanghai'
+
+GITHUB_URL = 'https://github.com/cfddream'
+BYLINE = '&copy; 2013 cfddream &amp; co. All Rights Reserved.'
+
+DEFAULT_LANG = u'cn'
+
+# Blogroll
+LINKS =  (
+	('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+        ('Python.org', 'http://python.org'),
+        ('Jinja2', 'http://jinja.pocoo.org'),)
+
+# Social widget
+SOCIAL = (
+	('GitHub', GITHUB_URL),
+	('Twitter', 'https://twitter.com/cfddream'),
+)
+
+DEFAULT_PAGINATION = 7

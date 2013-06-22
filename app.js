b072c57bf1b1d7a47afff174facbceaeb7267ded
cfddream <cfddream@gmail.com>
2013-06-22 15:51:46 +0800

    add editorconfig humans robots

--- app.js
+++ app.js
@@ -6,33 +6,76 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+
+  , config = require('./config')
+  , middleware = require('./lib/middleware');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+
+// -- Config -----------------------------------------------------------------
+
+app.set('name', 'Kissdry');
+app.set('env', config.env);
+app.set('port', config.port);
+app.enable('strict routing');
+
 app.set('view engine', 'jade');
+app.set('views', config.dirs.views);
+
+app.locals({
+    site              : 'Kissdry'
+  , copyright_year    : '2013'
+
+  , version           : config.version
+
+  , isDevelopment     : config.isDevelopment
+  , isProduction      : config.isProduction
+
+  // set layouts & partials basedir
+  , basedir           : config.dirs.base
+
+  , ga                : config.isProduction && config.ga
+});
+
+
+// -- Middleware --------------------------------------------------------------
+
 app.use(express.favicon());
+//app.use(express.favicon(path.join(config.dirs.pub, 'favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+//app.use(express.cookieParser('your secret here'));
+//app.use(express.session());
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(config.dirs.pub));
+
+// error handle  - 404
+app.use(middleware.notfound);
 
 // development only
-if ('development' == app.get('env')) {
+if (config.isDevelopment) {
   app.locals.compileDebug = true;
   app.locals.pretty = true;
-  app.use(express.errorHandler());
+  app.use(express.errorHandler({
+      dumpExceptions: true
+    , showStack     : true
+  }));
+} else {
+  app.use(middleware.error);
 }
+
+
+// -- Routes -----------------------------------------------------------------
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+
+// -- Server -----------------------------------------------------------------
+
+http.createServer(app).listen(config.port, function(){
+  console.log('Express server listening on port ' + config.port);
 });
